abb023bcadc39961f5fe970be9292f66fbe6cc32
Aivis Abele <aabele@gmail.com>
2017-05-25 10:56:27 +0300

    Fixed managers

--- omni_wagtail_events/managers.py
+++ omni_wagtail_events/managers.py
@@ -10,13 +10,23 @@ from django.db import models
 
 class AgendaItemsManager(models.Manager):
 
+    @staticmethod
+    def _get_min_time(dt):
+        """
+        Makes clock to 00:00:00
+        :param dt: datetime
+        :return: datetime
+        """
+        return dt.replace(hour=0, minute=0, second=0)
+
     def in_date_range(self, start, end):
         """
         Get event dates that appear between the start and end dates
-
         :return: Filtered django model queryset
         """
-        return self.get_queryset().filter(start_time__gte=start, end_time__lte=end)
+        start = self._get_min_time(start)
+        end = self._get_min_time(end)
+        return self.filter(start_time__gte=start, end_time__lte=end)
 
     def parent_ids_by_date(self, start, end):
         """
